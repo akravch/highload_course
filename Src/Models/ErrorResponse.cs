c58b90f13cd948f49f9a5a3e1577abd998905c2a
Alexander Kravchuk <akravch92@gmail.com>
2023-11-08 13:42:43 +0100

    Add ErrorResponse.

---
+++ Src/Models/ErrorResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace HighloadCourse.Models;
+
+public sealed class ErrorResponse
+{
+    public required string Message { get; init; }
+    public required string RequestId { get; init; }
+    public required int Code { get; init; }
+}
