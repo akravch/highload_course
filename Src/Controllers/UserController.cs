e3d8c855d54f286ae5a9cdee23015e59c9a77c8c
Alexander Kravchuk <akravch92@gmail.com>
2023-11-08 12:20:09 +0100

    Add UserController with empty routes.

---
+++ Src/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using HighloadCourse.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HighloadCourse.Controllers;
+
+[ApiController]
+public sealed class UserController : ControllerBase
+{
+    [HttpPost("/login")]
+    public async Task<UserLoginResponse> LoginAsync([FromBody] UserLoginRequest request)
+    {
+        return new UserLoginResponse { Token = "token" };
+    }
+
+    [HttpPost("/user/register")]
+    public async Task<UserRegisterResponse> RegisterAsync([FromBody] UserRegisterRequest request)
+    {
+        return new UserRegisterResponse { UserId = "id" };
+    }
+
+    [HttpGet("/user/get/{id}")]
+    public async Task<UserGetResponse> GetAsync([FromRoute] string id)
+    {
+        return new UserGetResponse
+        {
+            Id = "id",
+            FirstName = "first",
+            SecondName = "second",
+            Biography = "bio",
+            City = "city",
+            Birthdate = DateTimeOffset.Now
+        };
+    }
+}
