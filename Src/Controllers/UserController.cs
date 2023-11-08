548c3c3aaca4b8bd56d595320c4513cf672fdb64
Alexander Kravchuk <akravch92@gmail.com>
2023-11-08 13:43:50 +0100

    Annotate UserController with return types.

--- Src/Controllers/UserController.cs
+++ Src/Controllers/UserController.cs
@@ -7,18 +7,32 @@ namespace HighloadCourse.Controllers;
 public sealed class UserController : ControllerBase
 {
     [HttpPost("/login")]
+    [ProducesResponseType(typeof(UserLoginResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status500InternalServerError)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status503ServiceUnavailable)]
     public async Task<UserLoginResponse> LoginAsync([FromBody] UserLoginRequest request)
     {
         return new UserLoginResponse { Token = "token" };
     }
 
     [HttpPost("/user/register")]
+    [ProducesResponseType(typeof(UserRegisterResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status500InternalServerError)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status503ServiceUnavailable)]
     public async Task<UserRegisterResponse> RegisterAsync([FromBody] UserRegisterRequest request)
     {
         return new UserRegisterResponse { UserId = "id" };
     }
 
     [HttpGet("/user/get/{id}")]
+    [ProducesResponseType(typeof(UserGetResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status500InternalServerError)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status503ServiceUnavailable)]
     public async Task<UserGetResponse> GetAsync([FromRoute] string id)
     {
         return new UserGetResponse
