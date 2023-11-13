4669a30d95ed74b5a1f34a2f06e50aaf173c2227
Alexander Kravchuk <akravch92@gmail.com>
2023-11-13 17:48:08 +0100

    Add NpgsqlExtensions.

---
+++ Src/HighloadCourse/Utils/NpgsqlExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Npgsql;
+using NpgsqlTypes;
+
+namespace HighloadCourse.Utils;
+
+internal static class NpgsqlExtensions
+{
+    public static NpgsqlParameterCollection AddPositional<T>(
+        this NpgsqlParameterCollection collection,
+        T value,
+        NpgsqlDbType type,
+        int size)
+    {
+        collection.Add(new NpgsqlParameter<T>
+        {
+            TypedValue = value,
+            NpgsqlDbType = type,
+            Size = size
+        });
+
+        return collection;
+    }
+
+    public static NpgsqlParameterCollection AddPositional<T>(
+        this NpgsqlParameterCollection collection,
+        T value,
+        NpgsqlDbType type)
+    {
+        collection.Add(new NpgsqlParameter<T>
+        {
+            TypedValue = value,
+            NpgsqlDbType = type
+        });
+
+        return collection;
+    }
+}
