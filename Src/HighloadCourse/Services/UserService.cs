8ac1f38d76174168ae6461af173f4babf813d462
Alexander Kravchuk <akravch92@gmail.com>
2023-11-13 17:39:39 +0100

    Implement user/register to query database.

--- Src/HighloadCourse/Services/UserService.cs
+++ Src/HighloadCourse/Services/UserService.cs
@@ -1,4 +1,7 @@
-﻿using HighloadCourse.Models;
+﻿using System.Security.Cryptography;
+using HighloadCourse.Models;
+using HighloadCourse.Utils;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
 using Npgsql;
 using NpgsqlTypes;
 
@@ -26,8 +29,35 @@ public sealed class UserService : IDisposable
 
     public async Task<UserRegisterResponse> RegisterAsync(UserRegisterRequest request)
     {
-        return new UserRegisterResponse { UserId = "id" };
+        const string sql =
+            """
+            WITH account_insert AS (
+                INSERT INTO account (hash, salt)
+                VALUES (($1), ($2))
+                RETURNING id
+            )
+            INSERT INTO account_info (account_id, first_name, second_name, biography, city, birthdate)
+            VALUES ((SELECT id FROM account_insert), ($3), ($4), ($5), ($6), ($7))
+            RETURNING (SELECT id FROM account_insert)
+            """;
 
+        await using var command = _dataSource.CreateCommand(sql);
+
+        var salt = RandomNumberGenerator.GetBytes(32);
+        var hash = KeyDerivation.Pbkdf2(request.Password, salt, KeyDerivationPrf.HMACSHA256, 100_000, 64);
+
+        command.Parameters
+            .AddPositional(Convert.ToHexString(hash), NpgsqlDbType.Char, 128)
+            .AddPositional(Convert.ToHexString(salt), NpgsqlDbType.Char, 64)
+            .AddPositional(request.FirstName, NpgsqlDbType.Varchar, 128)
+            .AddPositional(request.SecondName, NpgsqlDbType.Varchar, 128)
+            .AddPositional(request.Biography, NpgsqlDbType.Varchar, 2048)
+            .AddPositional(request.City, NpgsqlDbType.Varchar, 128)
+            .AddPositional(request.Birthdate, NpgsqlDbType.Date);
+
+        var userId = (long) (await command.ExecuteScalarAsync())!;
+
+        return new UserRegisterResponse { UserId = userId.ToString() };
     }
 
     public async Task<UserGetResponse?> GetAsync(string id)
