4b8e570f76ad8dbb71ebf01194a207d313003231
Alexander Kravchuk <akravch92@gmail.com>
2023-11-19 15:09:46 +0100

    Use snake_case property naming policy for JSON serializer.

--- Src/HighloadCourse/Program.cs
+++ Src/HighloadCourse/Program.cs
@@ -1,8 +1,11 @@
+using System.Text.Json;
 using HighloadCourse.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Services
+    .AddControllers()
+    .AddJsonOptions(options => options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
