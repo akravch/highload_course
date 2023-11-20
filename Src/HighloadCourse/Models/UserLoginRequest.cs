3b5409cfb17e19ad036ae35d3f0f9ce0f85510fd
Alexander Kravchuk <akravch92@gmail.com>
2023-11-20 11:03:07 +0100

    Add validation annotations on requests.

--- Src/HighloadCourse/Models/UserLoginRequest.cs
+++ Src/HighloadCourse/Models/UserLoginRequest.cs
@@ -1,7 +1,12 @@
-﻿namespace HighloadCourse.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HighloadCourse.Models;
 
 public sealed class UserLoginRequest
 {
+    [Required]
     public required string Id { get; init; }
+
+    [Required]
     public required string Password { get; init; }
 }
--- Src/HighloadCourse/Models/UserRegisterRequest.cs
+++ Src/HighloadCourse/Models/UserRegisterRequest.cs
@@ -1,11 +1,28 @@
-﻿namespace HighloadCourse.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HighloadCourse.Models;
 
 public sealed class UserRegisterRequest
 {
+    [Required]
+    [MaxLength(128)]
     public required string FirstName { get; init; }
+
+    [Required]
+    [MaxLength(128)]
     public required string SecondName { get; init; }
+
+    [Required]
+    [MaxLength(2048)]
     public required string Biography { get; init; }
+
+    [Required]
+    [MaxLength(128)]
     public required string City { get; init; }
+
+    [Required]
     public required string Password { get; init; }
+
+    [Required]
     public required DateOnly Birthdate { get; init; }
 }
