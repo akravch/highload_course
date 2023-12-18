d5b21972cc2f4c09800f334c503f7689afd8a426
Alexander Kravchuk <akravch92@gmail.com>
2023-12-18 14:47:50 +0100

    Fix GIN index creation.

--- Scripts/create_tables.sql
+++ Scripts/create_tables.sql
@@ -18,4 +18,4 @@ CREATE TABLE IF NOT EXISTS account_info (
 
 CREATE EXTENSION IF NOT EXISTS pg_trgm;
 
-CREATE INDEX IF NOT EXISTS idx_account_info_second_name_first_name ON account_info USING GIN(second_name, first_name);
+CREATE INDEX IF NOT EXISTS idx_account_info_second_name_first_name ON account_info USING GIN(second_name gin_trgm_ops, first_name gin_trgm_ops);
